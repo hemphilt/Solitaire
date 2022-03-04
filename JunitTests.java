214131341747ae17a9d2b84a3ef6d742889408eb
olmsteak <95241451+olmsteak@users.noreply.github.com>
2022-03-04 04:01:16 -0500

    junit tests finished?

--- JunitTests.java
+++ JunitTests.java
@@ -1,4 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
+import sun.rmi.runtime.Log;
 
 import java.util.ArrayList;
 
@@ -303,6 +305,139 @@ public class JunitTests {
         assertNotSame(logic.getDeck(), deck); //compares the set deck to the original deck
         //This should be the same and is the same but will fail because they are different objects
         //So I changed it so that it would pass
+    }
+
+    /***************************************************************************************************
+     * This tests the newGame() and drawCard() functions in the Logic class
+     **************************************************************************************************/
+    @Test
+    public void testLogicNewGame() {
+        logic = new Logic();
+        logic.newGame(); //sets up a new game
+
+        assertEquals(logic.deck.deckSize(), 24);
+        //There should only be 24 cards left in the deck after it is laid out for the start of the game
+
+        logic.drawCard();
+        assertEquals(logic.deck.deckSize(), 23);
+        //drawing a card subtracts a card from the deck
+        assertEquals(logic.drawPile.getPileSize(), 1);
+        //drawing a card increase the draw Pile size by one
+    }
+
+    /************************************************************************************************
+     * This tests the shuffleWaste() function in the Logic class
+     ***********************************************************************************************/
+    @Test
+    public void testLogicShuffleWaste() {
+        logic = new Logic();
+        logic.newGame(); //sets up a new game
+        for(int i = 0; i < 24; i++) //transfers the deck to the drawPile
+            logic.drawCard();
+        assertTrue(logic.deck.isEmpty()); //makes sure the deck is empty
+        assertEquals(logic.drawPile.getPileSize(),24);
+        //makes sure the drawPile got all the cards from the deck
+
+        logic.shuffleWaste(); //transfers the drawPile back to the deck
+        assertEquals(logic.deck.deckSize(), 24);
+        //makes sure the deck gets all the cards back
+        assertEquals(logic.drawPile.getPileSize(), 0);
+        //makes sure all the cards are gone from the drawPile
+    }
+
+    /******************************************************************************************
+     * This tests the selectCard() function in the Logic class
+     ******************************************************************************************/
+    @Test
+    public void testLogicSelectCard() {
+        logic = new Logic();
+//        logic.newGame(); //sets up a new game
+        pile = new Pile(PileType.FOUNDATION); //starts a pile
+        card = new Card(Suit.HEARTS, Rank.ACE); //sets up a new card
+        pile.addCard(card); //adds a card to the pile
+
+        card1 = logic.selectCard(pile, 0); //selects the card in the pile
+        assertEquals(card, card1); //makes sure the card selected is the one in pile
+
+        card.setIsFlipped(false); //flips the card over
+        Pile pile1 = new Pile(PileType.TABLEAU);
+        pile1.addCard(card); //adds the card to the pile
+        card1 = logic.selectCard(pile1, 0); //selects the card from the pile
+        assertEquals(card, card1); //makes sure it can select the card
+
+
+        Pile pile2 = new Pile(PileType.WASTE);
+        pile2.addCard(card); //adds the card to the pile
+        card1 = logic.selectCard(pile2, 0); //selects the card from the pile
+        assertEquals(card, card1); // makes sure it can select the card
+
+        Pile pile3 = new Pile(PileType.TABLEAU);
+        card2 = new Card(Suit.CLUBS, Rank.KING);
+        pile3.addCard(card); //adds the first card to the pile
+        pile3.addCard(card2); //adds the second card to the pile
+        card1 = logic.selectCard(pile3, 1); //selects the card in the pile
+        assertEquals(card2, card1); //makes sure it selects the right card
+
+        Pile pile4 = new Pile(PileType.TABLEAU);
+        card.setIsFlipped(true); //flips the card back over
+        pile4.addCard(card); //adds the card to the pile
+        card1 = logic.selectCard(pile4, 0); //selects the first card in the pile
+        assertNull(card1); //makes sure nothing is selected
+    }
+
+
+    /***************************************************************************************
+     * This tests the movePile() function in the Logic class
+     **************************************************************************************/
+    @Test
+    public void testLogicMovePile() {
+        pile = new Pile(PileType.FOUNDATION);
+        card = new Card(Suit.HEARTS, Rank.ACE);
+        card2 = new Card(Suit.CLUBS, Rank.TWO);
+        Pile pile1 = new Pile(PileType.FOUNDATION);
+
+        pile1.addCard(card2); //adds the card to pile1
+        pile1.addCard(card); //adds the card to pile1
+        assertEquals(pile.getPileSize(), 0); //there should be no cards in pile
+        assertEquals(pile1.getPileSize(), 2); //there should be two cards in pile1
+
+        logic.movePile(pile1, pile, 1); //moves from one pile to another
+        assertEquals(pile.getPileSize(), 1); //there should now be one card in pile
+        assertEquals(pile1.getPileSize(), 1); //there should only be one card in pile1
+        assertEquals(pile.getCard(0), card); //making sure the correct card got transfered
+    }
+
+    /********************************************************************************
+     *
+     */
+    @Test
+    public void testLogicGameEnded1() {
+        logic = new Logic();
+        logic.newGame(); //sets up a new game
+
+        assertFalse(logic.gameEnded());
+        //There is still cards in the deck so the game should not end
+        for(int i = 0; i < 24; i++) //transfers the deck to the drawPile
+            logic.drawCard();
+        assertFalse(logic.gameEnded());
+        //There is still cards in the drawPile so the game should not end
+    }
+
+    @Test
+    public void testLogicGameEnded2() {
+        logic = new Logic();
+        logic.newGame();
+        pile = new Pile(PileType.WASTE);
+        tablePiles = new ArrayList<>(); //initializes the array
+
+        logic.deck.dealToPile(pile, 24); //gets rid of all the cards in the deck
+
+        assertFalse(logic.gameEnded()); //the game should not end yet
+
+        logic.setTablePiles(tablePiles); //causes the tablePiles to get flushed
+        assertTrue(logic.gameEnded()); //game should say that it is ended now
+
+
     }
 
 }
